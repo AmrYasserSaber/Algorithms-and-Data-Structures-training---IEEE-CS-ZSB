e264b88e496219488b89c0040dd33ca6bd63c664
AmrYasserSaber <amr.yassr.2@gmail.com>
2022-04-05 00:11:56 +0200

    *

--- Algorithmic Toolbox/Week2/6-LastFibonacci.py
+++ Algorithmic Toolbox/Week2/6-LastFibonacci.py
@@ -1,13 +1,16 @@
 def fibonacci(n):
     n1, n2 = 0, 1
     total = 1
-    for index in range(n - 1):
-        sum = (n1 + n2) % 10
-        n1 = n2
-        n2 = sum
-        total += sum
-    total = str(total)
-    return int(total[-1])
+    if n==0:
+        return(0)
+    else:
+        for index in range(n - 1):
+            sum = (n1 + n2) % 10
+            n1 = n2
+            n2 = sum
+            total += sum
+        total = str(total)
+        return int(total[-1])
 
 
 n = int(input())
